e71c4b7a393c3fe74eb4d992a73c6a219d6b15d7
unknown <zelalemyohannes75@gmail.com>
2024-03-18 16:22:20 +0300

    ss

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ApplicationRecord
-  belongs_to :author, class_name: 'User'
+  belongs_to :user
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   validates :title, presence: true, length: { maximum: 250 }
-  validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
-  validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :CommentsCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  # validates :LikesCounter, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
