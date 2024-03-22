c6a147a82072218f6738d460cba8e7fa8c517774
unknown <zelalemyohannes75@gmail.com>
2024-03-22 21:33:33 +0300

    add more

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,9 +14,12 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+
   # Enable server timing
   config.server_timing = true
-
+ 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join("tmp/caching-dev.txt").exist?
@@ -52,6 +55,7 @@ Rails.application.configure do
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
+  
 
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
