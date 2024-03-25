333e00415c2e7440e73c9406d485550680d844cb
unknown <zelalemyohannes75@gmail.com>
2024-03-25 21:58:29 +0300

    ss

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,14 @@
 # end
 
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
-    helper_method :current_user
   
-    private
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo])
+  end
   
-    def current_user
-      @current_user ||= User.first
-    end
 end
   
